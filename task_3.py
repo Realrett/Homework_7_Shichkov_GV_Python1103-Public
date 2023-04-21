d250507c60643ef211229e345209fa43d8bb63f0
Realrett <gosha-student@mail.ru>
2023-04-21 20:04:53 +0700

    task_3

---
+++ task_3.py
@@ -0,0 +1,46 @@
+
+# Задание 3.
+
+# Реализовать базовый класс Worker (работник),
+# в котором определить публичные атрибуты name, surname, position (должность),
+# и защищенный атрибут income (доход). Последний атрибут должен ссылаться
+# на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+
+# Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
+# получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+
+# Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+# проверить значения атрибутов, вызвать методы экземпляров).
+
+# П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку str
+# str(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
+
+
+
+class Worker:
+    _income = {"wage": 0,
+               "bonus": 0}
+
+    def __init__(self, name, surname, position):
+        self.name = name
+        self.surname = surname
+        self.position = position
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, income):
+        super().__init__(name, surname, position)
+        self._income = income
+
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+    def __str__(self):
+        return self.get_full_name() + "\n" + str(self.get_total_income())
+
+
+obj = Position("Ivan", "Ivanov", "worker", {'wage': 100, 'bonus': 50})
+print(obj)
